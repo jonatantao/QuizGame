46df67c3f290aea5602d888f92a11d8c5ad884a3
jonatantao <michelik.jony@gmail.com>
2023-12-10 22:15:06 +0100

    Grip\Trigger pre VR ruky

--- Assets/HandsAnimation.cs
+++ Assets/HandsAnimation.cs
@@ -6,12 +6,16 @@ using UnityEngine.InputSystem;
 public class HandsAnimation : MonoBehaviour
 {
     [SerializeField] private InputActionReference gripReference;
+    [SerializeField] private InputActionReference triggerReference;
     [SerializeField] private Animator handAnimator;
     
     // Update is called once per frame
     void Update()
     {
         float gripValue = gripReference.action.ReadValue<float>();
-        
+        handAnimator.SetFloat("Grip", gripValue);
+
+        float triggerValue = triggerReference.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
     }
 }
